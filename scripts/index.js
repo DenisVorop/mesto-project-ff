061026709da25de13f0e4d5867632f395bb964ea
DenisVorop <saskeazaske@icloud.com>
2023-11-18 16:39:18 +0300

    feat: cards are shown & user can remove each card

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,48 @@
-// @todo: Темплейт карточки
+// Темплейт карточки
 
-// @todo: DOM узлы
+const cardTemplate = document.getElementById("card-template");
 
-// @todo: Функция создания карточки
+// DOM узлы
 
-// @todo: Функция удаления карточки
+const cardsContainer = document.querySelector(".places__list");
+const addCardButton = document.querySelector(".profile__add-button");
 
-// @todo: Вывести карточки на страницу
+// Функция создания карточки
+
+const createCard = ({ name, link }) => {
+  const cardTemplateContent = cardTemplate.content;
+  const cardNode = cardTemplateContent.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardNode.querySelector(".card__image");
+  const cardTitle = cardNode.querySelector(".card__title");
+
+  cardImage.src = link;
+  cardImage.alt = `на изображении ${name}`;
+
+  cardTitle.textContent = name;
+
+  cardsContainer.append(cardNode);
+};
+
+// Функция удаления карточки
+
+const removeCard = (e) => {
+  const removeButton = e.target.closest(".card__delete-button");
+
+  if (!removeButton) return;
+
+  const card = e.target.closest(".card");
+  card.remove();
+};
+
+cardsContainer.addEventListener("click", removeCard);
+
+// Вывести карточки на страницу
+
+const initCards = (cards) => {
+  cards.forEach((card) => {
+    createCard(card);
+  });
+};
+
+initCards(initialCards);
