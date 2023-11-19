3b26a40faa4e34d7ee43820f6eafd8fb3ac5a8b4
DenisVorop <saskeazaske@icloud.com>
2023-11-19 15:50:00 +0300

    fix: resolve all issues

--- scripts/index.js
+++ scripts/index.js
@@ -9,39 +9,36 @@ const addCardButton = document.querySelector(".profile__add-button");
 
 // Функция создания карточки
 
-const createCard = ({ name, link }) => {
+const createCard = ({ name, link }, callback) => {
   const cardTemplateContent = cardTemplate.content;
   const cardNode = cardTemplateContent.querySelector(".card").cloneNode(true);
 
   const cardImage = cardNode.querySelector(".card__image");
   const cardTitle = cardNode.querySelector(".card__title");
+  const removeButton = cardNode.querySelector(".card__delete-button");
 
   cardImage.src = link;
   cardImage.alt = `на изображении ${name}`;
 
   cardTitle.textContent = name;
 
-  cardsContainer.append(cardNode);
+  removeButton.addEventListener('click', callback)
+
+  return cardNode;
 };
 
 // Функция удаления карточки
 
 const removeCard = (e) => {
-  const removeButton = e.target.closest(".card__delete-button");
-
-  if (!removeButton) return;
-
   const card = e.target.closest(".card");
   card.remove();
 };
-
-cardsContainer.addEventListener("click", removeCard);
 
 // Вывести карточки на страницу
 
 const initCards = (cards) => {
   cards.forEach((card) => {
-    createCard(card);
+    cardsContainer.append(createCard(card, removeCard));
   });
 };
 
